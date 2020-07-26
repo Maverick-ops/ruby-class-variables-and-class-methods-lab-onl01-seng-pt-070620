410e7a5d39dec2b01826a5d6dcaa87ee4e5924d8
Jesse Murphy <jessemurphy1982@gmail.com>
2020-07-26 18:35:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,6 @@
 class Song 
 attr_accessor :name, :artist, :genre 
-  @@song_count = 0 
+  @@count = 0 
   @@genres = []
   @@artists = []
   
@@ -10,11 +10,11 @@ attr_accessor :name, :artist, :genre
     @genre = genre
     @@genres << @genre 
     @@artists << @artist
-    @@song_count +=1 
+    @@count +=1 
   end
   
   def self.count 
-    @@song_count
+    @@count
   end
   
   def self.genres
